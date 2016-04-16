aa3d2a97bcef403589b5c1e3d347b16107721933
luze <huangx@chemao.com.cn>
2016-04-16 19:04:08 +0800

    use spring-boot web

---
+++ bakon-web/src/test/java/cn/bakon/web/WebApplicationTest.java
@@ -0,0 +1,34 @@
+package cn.bakon.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.bakon.web.WebApplication;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(WebApplication.class)
+@WebIntegrationTest(randomPort = true)
+@DirtiesContext
+public class WebApplicationTest {
+	@Value("${local.server.port}")
+	private int port;
+
+	@Test
+	public void testCss() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + this.port + "/css/bootstrap.min.css", String.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertTrue("Wrong body:\n" + entity.getBody(), entity.getBody().contains("body"));
+	}
+}
