e0865358cefd4f375901a172396002b28d9b8d5a
luze <huangx@chemao.com.cn>
2016-04-18 00:39:44 +0800

    add web controller

--- bakon-web/src/main/java/cn/bakon/web/HomeController.java
+++ bakon-web/src/main/java/cn/bakon/web/HomeController.java
@@ -1,9 +1,18 @@
 package cn.bakon.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
+	@Autowired
+	private EquipmentRepository equipmentRepository;
+
+	@RequestMapping("/monitor")
+	public ModelAndView monitor() {
+		this.equipmentRepository.findAll();
+		return new ModelAndView("monitor");
+	}
 }
---
+++ bakon-web/src/test/java/cn/bakon/web/HomeControllerTest.java
@@ -0,0 +1,47 @@
+package cn.bakon.web;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import cn.bakon.WebApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(WebApplication.class)
+@WebAppConfiguration
+public class HomeControllerTest {
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+	@Test
+	public void testMonitor() throws Exception {
+		this.mvc.perform(get("/monitor"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("html")))
+				.andDo(new ResultHandler() {
+					@Override
+					public void handle(MvcResult result) throws Exception {
+					}
+				});
+	}
+}
