dabdb361476fe2ea6621d96494dcb90198768500
luze <huangx@chemao.com.cn>
2016-04-17 20:37:48 +0800

    update tests; cleanup properties

--- bakon-web/src/main/java/cn/bakon/WebApplication.java
+++ bakon-web/src/main/java/cn/bakon/WebApplication.java
@@ -8,5 +8,4 @@ public class WebApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
 }
---
+++ bakon-web/src/test/java/cn/bakon/RestApplicationTest.java
@@ -0,0 +1,38 @@
+package cn.bakon;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(WebApplication.class)
+@WebAppConfiguration
+public class RestApplicationTest {
+	@Autowired
+	private WebApplicationContext context;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+	@Test
+	public void testHome() throws Exception {
+		this.mvc.perform(get("/api"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("equipments")));
+	}
+}
