6661532342b364e0e32dd64cb81a755e8475599a
wsky <wskyhx@gmail.com>
2016-05-08 23:39:42 +0800

    add queryDSL;findAllStatus by predicate

--- bakon-web/src/main/java/cn/bakon/web/EquipmentStatusRepository.java
+++ bakon-web/src/main/java/cn/bakon/web/EquipmentStatusRepository.java
@@ -2,13 +2,16 @@ package cn.bakon.web;
 
 import java.util.List;
 
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import cn.bakon.domain.EquipmentStatus;
 
 @RepositoryRestResource(collectionResourceRel = "status", path = "status")
-public interface EquipmentStatusRepository extends PagingAndSortingRepository<EquipmentStatus, Long> {
+public interface EquipmentStatusRepository extends
+		PagingAndSortingRepository<EquipmentStatus, Long>,
+		QueryDslPredicateExecutor<EquipmentStatus> {
 	// 查询当前所有状态
 	List<EquipmentStatus> findByCurrent(boolean current);
 }
--- bakon-web/src/main/java/cn/bakon/web/HomeController.java
+++ bakon-web/src/main/java/cn/bakon/web/HomeController.java
@@ -3,12 +3,15 @@ package cn.bakon.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.mysema.query.types.Predicate;
 
 import cn.bakon.domain.Equipment;
 import cn.bakon.domain.EquipmentStatus;
@@ -37,5 +40,10 @@ public class HomeController {
 	@ResponseBody
 	public List<EquipmentStatus> getAllStatus() {
 		return this.statusRepository.findByCurrent(true);
+	}
+	
+	@RequestMapping(value = "/api/getAllStatus", method = RequestMethod.GET)
+	public Iterable<EquipmentStatus> findAllStatus(@QuerydslPredicate Predicate predicate) {
+		return this.statusRepository.findAll(predicate);
 	}
 }
